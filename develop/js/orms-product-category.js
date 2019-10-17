c068ecf18edfda25adf0313c4e47098770d57de2
Darth Vader <pozoltd@gmail.com>
2019-10-17 22:37:01 +1300

    yes

--- develop/js/orms-product-category.js
+++ develop/js/orms-product-category.js
@@ -2,6 +2,15 @@ $(function() {
 
     //closed
     $(document).on('click', '.js-update-category', function () {
+        var container = $('<div>').html('<h3>It will take a while, please wait...</h3>').appendTo('body');
+        container.dialog({
+            title: 'Updating',
+            resizable: false,
+            draggable: false,
+            modal: true,
+            dialogClass: 'confirmation',
+        });
+
         $.ajax({
             type: 'GET',
             url: '/manage/rest/product-categories',
