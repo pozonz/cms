08d87f00b26a282bea4f9fbc7595de72e9411c15
Weida <weida@gravitate.co.nz>
2021-10-14 16:06:39 +1300

    add filename to download

--- develop/js/orm.js
+++ develop/js/orm.js
@@ -15,7 +15,7 @@ $(function () {
         toggle: function () {
             window._redactor = this;
             window._callback = function () {
-                window._redactor.app.insertion.insertHtml('<a target="_blank" href="/downloads/assets/' + $(this).closest('.js-orm-info').data('id') + '">' + $(this).closest('.js-orm-info').find('a').attr('title') + '</a>');
+                window._redactor.app.insertion.insertHtml('<a target="_blank" href="/downloads/assets/' + $(this).closest('.js-orm-info').data('id') + ($(this).closest('.js-orm-info').data('filename') ? ('/' + encodeURIComponent($(this).closest('.js-orm-info').data('filename'))) : '') + '">' + $(this).closest('.js-orm-info').find('a').attr('title') + '</a>');
                 $.fancybox.close();
             };
             filepicker(null);
